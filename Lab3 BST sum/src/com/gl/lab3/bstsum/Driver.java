a0ec018dfadc3f859b9e81ca7c794c82133f7e5a
publicvoidProtyush <120802951+publicvoidProtyush@users.noreply.github.com>
2023-01-30 21:52:13 +0530

    Add files via upload

---
+++ Lab3 BST sum/src/com/gl/lab3/bstsum/Driver.java
@@ -0,0 +1,24 @@
+package com.gl.lab3.bstsum;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		
+		Node root = null;
+		
+		FindSumPair findSP  = new FindSumPair();
+		
+		root  = findSP.insert(root, 40);
+		root  = findSP.insert(root, 20);
+		root  = findSP.insert(root, 60);
+		root  = findSP.insert(root, 10);
+		root  = findSP.insert(root, 30);
+		root  = findSP.insert(root, 50);
+		root  = findSP.insert(root, 70);
+		
+		int sum = 70;
+		findSP.findPairWithGivenSum(root, sum);
+	}
+
+}
---
+++ Lab3 BST sum/src/com/gl/lab3/bstsum/Node.java
@@ -0,0 +1,15 @@
+package com.gl.lab3.bstsum;
+
+public class Node {
+
+	    int data;
+	    
+	    Node leftNode, rightNode;
+	    
+	    public Node(int data) {
+	    	
+	    	this.data = data;
+	    	
+	    	leftNode = rightNode = null;
+	    }
+}
